c00650e4c70ce3109c8e6cda8a2b604a12d695b7
divyaraj <divyarajdeepak2356@gmail.com>
2025-03-04 00:42:17 +0530

    AI/ML task 1 -  commiting all files related to task 1

---
+++ Readme.md
@@ -0,0 +1,38 @@
+### AI-ML-Task-Image-Classification-Challenge:
+
+#### Level - 1: Exploratory Data Analysis
+
+
+> Dataset is not present is in this repository as it was too large to upload to github.
+
+
+##### Steps: 
+
+- Step - 1: Creating main.py file and importing the required libraries (pandas, numpy and matplotlib).
+
+![step1](images/step1.png)
+
+- Step - 2: Reading the csv data file using pandas and storing it in a dataframe
+
+![step2](images/step2.png)
+
+- Step - 3: Using the dataframe classify the images based on their labels into a dictionary. Create subplots for displaying the images.
+
+![step3](images/step3.png)
+
+- Step - 4: Display the sample images (first) from each particular label category to understand the data distribution.
+
+![step4](images/step4.png)
+
+- Step - 5: Print the summary statistics of the given datset.
+
+![step5](images/step5.png)
+
+
+##### Plot diplsaying sample images:
+
+![plot](images/plot1.png)
+
+##### Summary/Statistics of the dataset:
+
+![summary statistics](images/summary_statistics.png)
---
+++ main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings as wr
+wr.filterwarnings('ignore')
+
+
+df = pd.read_csv("dataset_aiml_task/data.csv")
+
+labels = df.iloc[:, 0].unique()
+label_dfs = {label: df[df.iloc[:, 0] == label] for label in labels} # Classify the unique labels into a dictionary
+print(df.iloc[:, 0].value_counts())  # Count images per label
+
+image_size = int(np.sqrt(df.shape[1] - 1)) # square 28x28 images are there in the dataset
+
+# create 10 subplots for 2 row and 5 columns for each sample image
+fig, axes = plt.subplots(2, 5, figsize=(12, 6))
+
+# convert to 1D flattened array using numpy
+axes = axes.ravel()
+
+
+# loop and show the first image of from each label to show sample images
+for i, label in enumerate(sorted(labels)):
+    sample_image = label_dfs[label].iloc[0, 1:].values.reshape(image_size, image_size)
+    axes[i].imshow(sample_image, cmap="gray")
+    axes[i].set_title(f'Label {label}')
+    axes[i].axis("off")
+
+plt.tight_layout()
+plt.show()
+
+print(df.describe())
